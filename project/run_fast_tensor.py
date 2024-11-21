0becdd7915c50620cd16c07c72cc3faf94dc11f2
Wenxuan Yu <wy@dhcp-vl2051-136.eduroam.cornell.edu>
2024-11-20 22:34:44 -0500

    fixed bug

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 import numba
 
 import minitorch
@@ -10,8 +10,8 @@ if numba.cuda.is_available():
     GPUBackend = minitorch.TensorBackend(minitorch.CudaOps)
 
 
-def default_log_fn(epoch, total_loss, correct, losses):
-    print("Epoch ", epoch, " loss ", total_loss, "correct", correct)
+def default_log_fn(epoch, total_loss, correct, losses, epoch_time):
+    print("Epoch ", epoch, " loss ", total_loss, "correct", correct, "time", epoch_time)
 
 
 def RParam(*shape, backend):
@@ -47,6 +47,8 @@ class Linear(minitorch.Module):
         return x @ self.weights.value + self.bias.value
 
 
+
+
 class FastTrain:
     def __init__(self, hidden_layers, backend=FastTensorBackend):
         self.hidden_layers = hidden_layers
@@ -66,6 +68,7 @@ class FastTrain:
         losses = []
 
         for epoch in range(max_epochs):
+            start_time = time.time()  # Start timer for the epoch
             total_loss = 0.0
             c = list(zip(data.X, data.y))
             random.shuffle(c)
@@ -95,7 +98,9 @@ class FastTrain:
                 out = self.model.forward(X).view(y.shape[0])
                 y2 = minitorch.tensor(data.y)
                 correct = int(((out.detach() > 0.5) == y2).sum()[0])
-                log_fn(epoch, total_loss, correct, losses)
+                epoch_time = time.time() - start_time  # Calculate epoch duration
+                log_fn(epoch, total_loss, correct, losses, epoch_time)
+
 
 
 if __name__ == "__main__":
